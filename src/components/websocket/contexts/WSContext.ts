904392675505cdc2ba3b93a9cbb8977ec0ccfe70
capyq <quentin.capy@rte-france.com>
2024-07-11 11:12:30 +0200

    add common Websocket listener. We need 3items ( context / component / custom hook )
    Signed-off-by: capyq <quentin.capy@rte-france.com>

---
+++ src/components/websocket/contexts/WSContext.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { createContext } from 'react';
+
+export type ListenerWS = {
+    id: string;
+    callback: (event: MessageEvent) => void;
+};
+
+export type WSContextType = {
+    addListener: (url: string, l: ListenerWS) => void;
+    removeListener: (url: string, idListener: string) => void;
+};
+
+export type WSContextRecordType = Record<string, WSContextType>;
+
+export const WSContext = createContext<WSContextType>({
+    addListener: () => {},
+    removeListener: () => {},
+});
