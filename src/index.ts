0d89d0e379101cec66a1fbf7c982a8286334cd0a
dbraquart <107846716+dbraquart@users.noreply.github.com>
2024-08-14 12:39:28 +0200

    CustomMuiDialog can have a confirmation dialog (#548)
    use case : print a warning in a confirmation pop-up while importing data into open-source env like "demo".
    
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/index.ts
+++ src/index.ts
@@ -163,6 +163,7 @@ export { default as useIntlRef } from './hooks/useIntlRef';
 export { useSnackMessage } from './hooks/useSnackMessage';
 export { default as useDebounce } from './hooks/useDebounce';
 export { default as usePrevious } from './hooks/usePrevious';
+export { default as useConfidentialityWarning } from './hooks/useConfidentialityWarning';
 export { default as SelectClearable } from './components/inputs/select-clearable';
 export { default as useCustomFormContext } from './components/inputs/react-hook-form/provider/use-custom-form-context';
 export { default as CustomFormProvider } from './components/inputs/react-hook-form/provider/custom-form-provider';
