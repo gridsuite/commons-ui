14affdfc6bb78dce5c06d5f3ea89faea7181e62b
basseche <bassel.el-cheikh@rte-france.com>
2024-09-25 11:39:20 +0200

    Reduce list height and add autocomplete to field select component

---
+++ src/components/inputs/reactQueryBuilder/FieldSelector.tsx
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { FullOption, toFlatOptionArray, ValueSelectorProps } from 'react-querybuilder';
+import { Autocomplete, TextField } from '@mui/material';
+
+function FieldSelector(props: ValueSelectorProps) {
+    const { options, className, value, disabled, handleOnChange } = props;
+    const optionList: FullOption[] = toFlatOptionArray(options);
+
+    return (
+        <Autocomplete
+            onChange={(event, newValue) => {
+                handleOnChange(newValue?.name);
+            }}
+            value={optionList.find((option) => option.name === value)}
+            disabled={disabled}
+            className={className}
+            options={optionList}
+            size="small"
+            renderInput={(params) => <TextField {...params} label="" variant="outlined" />}
+            getOptionLabel={(option) => option.label}
+        />
+    );
+}
+export default FieldSelector;
