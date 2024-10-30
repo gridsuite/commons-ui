2231f4e77db7cd818235121413db085b3f0d099f
basseche <bassel.el-cheikh@rte-france.com>
2024-10-30 13:28:31 +0100

    review 2

--- src/components/inputs/reactQueryBuilder/FieldSelector.tsx
+++ src/components/inputs/reactQueryBuilder/FieldSelector.tsx
@@ -9,24 +9,18 @@ import { ValueSelectorProps } from 'react-querybuilder';
 import { MaterialValueSelector } from '@react-querybuilder/material';
 
 const ITEM_HEIGHT = 32;
+const ITEM_PADDING = 4;
 const ITEMS_COUNT = 6;
-const ITEM_PADDING = 8;
 
 const MenuProps = {
     PaperProps: {
         style: {
-            maxHeight: (ITEM_HEIGHT + ITEM_PADDING) * ITEMS_COUNT - ITEM_PADDING,
+            maxHeight: (ITEM_HEIGHT + ITEM_PADDING) * ITEMS_COUNT + ITEM_PADDING,
         },
     },
 };
 
-export const optionStyle = {
-    option: {
-        height: ITEM_HEIGHT,
-        padding: ITEM_PADDING,
-    },
-};
-
 export function FieldSelector(props: ValueSelectorProps) {
-    return <MaterialValueSelector {...props} MenuProps={MenuProps} sx={optionStyle} />;
+    console.log('props', props);
+    return <MaterialValueSelector {...props} MenuProps={MenuProps} />;
 }
