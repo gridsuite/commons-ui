818dc509b805615bd641002317bbe5bf59188725
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-12-11 18:16:30 +0100

    add licence

--- src/utils/conversionUtils.ts
+++ src/utils/conversionUtils.ts
@@ -1,11 +1,11 @@
-import { FieldType } from './constants';
-
 /**
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+import { FieldType } from './constants';
+
 export const GRIDSUITE_DEFAULT_PRECISION: number = 13;
 
 export const roundToPrecision = (num: number, precision: number) => Number(num.toPrecision(precision));
