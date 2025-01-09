db1cdc39e4f16db0889ae2caeeb5274de2206572
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-01-09 11:00:56 +0100

    Allow editing description from filter edition dialogs (#660)
    * Allow editing description from filter edition dialogs
    
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/utils/constants/uiConstants.ts
+++ src/utils/constants/uiConstants.ts
@@ -1,10 +1,8 @@
-/**
- * Copyright (c) 2024, RTE (http://www.rte-france.com)
+/*
+ * Copyright © 2025, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-export * from './browserConstants';
-export * from './fetchStatus';
-export * from './fieldConstants';
-export * from './uiConstants';
+
+export const MAX_CHAR_DESCRIPTION = 500;
