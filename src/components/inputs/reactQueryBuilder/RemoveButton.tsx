32be813898522fa2b905eb399a77a11c19d72b04
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-01-27 17:47:09 +0100

    fix remove buttons
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/components/inputs/reactQueryBuilder/RemoveButton.tsx
+++ src/components/inputs/reactQueryBuilder/RemoveButton.tsx
@@ -10,7 +10,6 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useController } from 'react-hook-form';
 
-import { useState } from 'react';
 import { recursiveRemove } from '../../filter/expert/expertFilterUtils';
 
 const EXPERT_FILTER_QUERY = 'rules';
@@ -21,32 +20,15 @@ export function RemoveButton(props: ActionWithRulesProps) {
         field: { value: query, onChange },
     } = useController({ name: EXPERT_FILTER_QUERY });
 
-    const [visible, setVisible] = useState(false);
-
     function handleDelete() {
         // We don't want groups with no rules
         // So if we have only empty subgroups above the removed rule, we want to remove all of them
         onChange(recursiveRemove(query, path));
     }
 
-    function handleOnMouseOver() {
-        setVisible(true);
-    }
-
-    function handleOnMouseOut() {
-        setVisible(false);
-    }
-
     return (
-        <IconButton
-            size="small"
-            onClick={() => handleDelete()}
-            className={className}
-            title={title}
-            onMouseOver={() => handleOnMouseOver()}
-            onMouseOut={() => handleOnMouseOut()}
-        >
-            <DeleteIcon visibility={visible ? 'visible' : 'hidden'} />
+        <IconButton size="small" onClick={() => handleDelete()} className={className} title={title}>
+            <DeleteIcon />
         </IconButton>
     );
 }
