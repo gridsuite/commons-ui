49c589a9464cbf067c4e5b686e50f483b4322b53
Mathieu DEHARBE <mathieu.deharbe@rte-france.com>
2025-02-18 15:11:02 +0100

    clean
    Signed-off-by: Mathieu DEHARBE <mathieu.deharbe@rte-france.com>

--- src/components/filter/FilterForm.tsx
+++ src/components/filter/FilterForm.tsx
@@ -7,7 +7,7 @@
 
 import { Box } from '@mui/material';
 import { useFormContext, useWatch } from 'react-hook-form';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { HeaderFilterForm, FilterFormProps } from './HeaderFilterForm';
 import { FieldConstants } from '../../utils/constants/fieldConstants';
 import { ExplicitNamingFilterForm } from './explicitNaming/ExplicitNamingFilterForm';
