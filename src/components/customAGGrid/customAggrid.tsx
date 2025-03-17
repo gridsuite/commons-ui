41b2d648f0621e9f4ba9385a7c5c028ae8bc28a6
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2025-03-17 12:31:48 +0100

    refactor

--- src/components/customAGGrid/customAggrid.tsx
+++ src/components/customAGGrid/customAggrid.tsx
@@ -5,70 +5,67 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useCallback } from 'react';
-import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
+import { forwardRef, useCallback } from 'react';
+import { AgGridReact, type AgGridReactProps } from 'ag-grid-react';
 import { useIntl } from 'react-intl';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
-import { ColumnResizedEvent, GetLocaleTextParams } from 'ag-grid-community';
+import type { ColumnResizedEvent, GetLocaleTextParams } from 'ag-grid-community';
 import { Box, type SxProps, type Theme, useTheme } from '@mui/material';
 import { mergeSx } from '../../utils/styles';
 import { CUSTOM_AGGRID_THEME, styles } from './customAggrid.style';
 
-interface CustomAGGGridStyleProps {
+export type CustomAGGridProps<TData = any> = AgGridReactProps<TData> & {
     shouldHidePinnedHeaderRightBorder?: boolean;
     showOverlay?: boolean;
-}
-
-export interface CustomAGGridProps extends AgGridReactProps, CustomAGGGridStyleProps {}
-
-// We have to define a minWidth to column to activate this feature
-const onColumnResized = (params: ColumnResizedEvent) => {
-    const { column, finished } = params;
-    const colDefinedMinWidth = column?.getColDef()?.minWidth;
-    if (column && colDefinedMinWidth && finished) {
-        const newWidth = column?.getActualWidth();
-        if (newWidth < colDefinedMinWidth) {
-            params.api.setColumnWidths([{ key: column, newWidth: colDefinedMinWidth }], finished, params.source);
-        }
-    }
 };
 
-export const CustomAGGrid = React.forwardRef<AgGridReact, CustomAGGridProps>((props, ref) => {
-    const { shouldHidePinnedHeaderRightBorder = false, showOverlay = false, ...agGridReactProps } = props;
-    const theme = useTheme<Theme>();
-    const intl = useIntl();
+// We have to define a minWidth to column to activate this feature
+function onColumnResized({ api, column, finished, source }: ColumnResizedEvent) {
+    if (column) {
+        const colDefinedMinWidth = column.getColDef().minWidth;
+        if (colDefinedMinWidth && finished && column.getActualWidth() < colDefinedMinWidth) {
+            api.setColumnWidths([{ key: column, newWidth: colDefinedMinWidth }], finished, source);
+        }
+    }
+}
 
-    const GRID_PREFIX = 'grid.';
+export const CustomAGGrid = forwardRef<AgGridReact, CustomAGGridProps>(
+    ({ shouldHidePinnedHeaderRightBorder = false, showOverlay = false, ...agGridReactProps }, ref) => {
+        const theme = useTheme<Theme>();
+        const intl = useIntl();
 
-    const getLocaleText = useCallback(
-        (params: GetLocaleTextParams) => {
-            const key = GRID_PREFIX + params.key;
-            return intl.formatMessage({
-                id: key,
-                defaultMessage: params.defaultValue,
-            });
-        },
-        [intl]
-    );
+        const GRID_PREFIX = 'grid.';
 
-    return (
-        <Box
-            sx={mergeSx(
-                styles.grid as SxProps | undefined,
-                shouldHidePinnedHeaderRightBorder ? styles.noBorderRight : undefined,
-                showOverlay ? (styles.overlayBackground as SxProps | undefined) : undefined
-            )}
-            className={`${theme.aggrid.theme} ${CUSTOM_AGGRID_THEME}`}
-        >
-            <AgGridReact
-                ref={ref}
-                getLocaleText={getLocaleText}
-                onColumnResized={onColumnResized}
-                enableCellTextSelection
-                theme="legacy"
-                {...agGridReactProps}
-            />
-        </Box>
-    );
-});
+        const getLocaleText = useCallback(
+            (params: GetLocaleTextParams) => {
+                const key = GRID_PREFIX + params.key;
+                return intl.formatMessage({
+                    id: key,
+                    defaultMessage: params.defaultValue,
+                });
+            },
+            [intl]
+        );
+
+        return (
+            <Box
+                sx={mergeSx(
+                    styles.grid as SxProps | undefined,
+                    shouldHidePinnedHeaderRightBorder ? styles.noBorderRight : undefined,
+                    showOverlay ? (styles.overlayBackground as SxProps | undefined) : undefined
+                )}
+                className={`${theme.aggrid.theme} ${CUSTOM_AGGRID_THEME}`}
+            >
+                <AgGridReact
+                    ref={ref}
+                    getLocaleText={getLocaleText}
+                    onColumnResized={onColumnResized}
+                    enableCellTextSelection
+                    theme="legacy"
+                    {...agGridReactProps}
+                />
+            </Box>
+        );
+    }
+);
