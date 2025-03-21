4af726a7e61e4d9897a1470b55eec48d248d8798
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2025-03-21 10:04:41 +0100

    fix

--- src/translations/en/external/exportParamsEn.ts
+++ src/translations/en/external/exportParamsEn.ts
@@ -1,11 +1,12 @@
-import { defineMessages } from '../../utils';
-
 /**
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import { defineMessages } from '../../utils';
+
 export const exportParamsEn = defineMessages({
     'iidm.export.cgmes.base-name': { defaultMessage: 'Base name' },
     'iidm.export.cgmes.base-name.desc': { defaultMessage: 'Basename for output files' },
--- src/translations/fr/external/importParamsFr.ts
+++ src/translations/fr/external/importParamsFr.ts
@@ -1,11 +1,12 @@
-import { defineMessages } from '../../utils';
-
 /**
  * Copyright (c) 2024, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import { defineMessages } from '../../utils';
+
 export const importParamsFr = defineMessages({
     'iidm.import.cgmes.allow-unsupported-tap-changers': {
         defaultMessage: "Autoriser n'importe quel type de régleur ou déphaseur",
