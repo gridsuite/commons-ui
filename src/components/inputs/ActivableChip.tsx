7550dcbe9893f24bfd74c393ae2159d2b6b6bc71
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-04-02 16:09:37 +0200

    feat: add activable chip
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

---
+++ src/components/inputs/ActivableChip.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { Chip, Tooltip } from '@mui/material';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import CancelIcon from '@mui/icons-material/Cancel';
+
+export interface ActivableChipProps {
+    isActivated: boolean;
+    label: string;
+    tooltipMessage: string;
+    onClick: () => void;
+    isDisabled?: boolean;
+}
+
+export function ActivableChip(props: ActivableChipProps) {
+    const { isActivated, label, tooltipMessage, onClick, isDisabled } = props;
+
+    return (
+        <Tooltip title={tooltipMessage} arrow>
+            <span>
+                <Chip
+                    label={label}
+                    deleteIcon={isActivated ? <CheckCircleOutlineIcon /> : <CancelIcon />}
+                    color="primary"
+                    size="small"
+                    variant={isActivated ? 'filled' : 'outlined'}
+                    onDelete={onClick}
+                    onClick={onClick}
+                    disabled={isDisabled}
+                />
+            </span>
+        </Tooltip>
+    );
+}
