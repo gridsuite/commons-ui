6daa11e2027d798cea0618e1a867edc26918e9af
David BRAQUART <david.braquart@rte-france.com>
2025-06-10 17:17:57 +0200

    little rename
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/parameters/common/index.ts
+++ src/components/parameters/common/index.ts
@@ -7,7 +7,7 @@
 
 export * from './computing-type';
 export * from './constant';
-export * from './interface';
+export * from './parameters-edition-dialog-props';
 export * from './line-separator';
 export * from './parameters';
 export * from './parameters-creation-dialog';
