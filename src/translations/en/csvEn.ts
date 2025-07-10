b0cfc9d1b745c148026ffe1ef48673d26bff64db
David BRAQUART <david.braquart@rte-france.com>
2025-07-10 18:48:38 +0200

    use template not skeleton
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/translations/en/csvEn.ts
+++ src/translations/en/csvEn.ts
@@ -9,9 +9,9 @@ export const csvEn = {
     ImportCSV: 'Import CSV',
     noDataInCsvFile: 'No data found in this file',
     wrongCsvHeadersError:
-        'This CSV file has the wrong headers. Use Generate CSV Skeleton button to get supported CSV format',
+        'This CSV file has the wrong headers. Use Generate CSV template button to get supported CSV format',
     keepCSVDataMessage: 'Do you want to replace or add the new data to the current list ?',
-    GenerateCSV: 'Generate CSV skeleton',
+    GenerateCSV: 'Generate CSV template',
     UploadCSV: 'Upload CSV',
     uploadMessage: ' No file selected',
 };
