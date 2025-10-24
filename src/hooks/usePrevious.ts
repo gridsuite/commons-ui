c5da575d48a7b9f2df47fc55e1cf2c46613bbcdf
David BRAQUART <david.braquart@rte-france.com>
2025-10-24 12:54:50 +0200

    Revert "Upgrade react and react-dom to version 19 (#886)"
    This reverts commit b0c8434882df323c70b3065066e543ff84cd5dad.

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -7,7 +7,7 @@
 import { useEffect, useRef } from 'react';
 
 export function usePrevious<T = undefined>(value: T) {
-    const ref = useRef<T>(undefined);
+    const ref = useRef<T>();
     useEffect(() => {
         ref.current = value;
     }, [value]);
