a8939d9d9dbc2b62328a3c461593f527fac63325
Thang PHAM <phamthang37@gmail.com>
2025-11-06 18:31:58 +0100

    Correct position of submenus
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/components/menus/custom-nested-menu.tsx
+++ src/components/menus/custom-nested-menu.tsx
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { PropsWithChildren, useEffect, useRef, useState } from 'react';
+import { PropsWithChildren, useCallback, useRef, useState } from 'react';
 import { NestedMenuItem, NestedMenuItemProps } from 'mui-nested-menu';
 import { Box, MenuItem, type MenuItemProps } from '@mui/material';
 import { mergeSx, type MuiStyles, type SxStyle } from '../../utils/styles';
@@ -30,42 +30,36 @@ interface CustomNestedMenuItemProps extends PropsWithChildren, Omit<NestedMenuIt
 
 export function CustomNestedMenuItem({ sx, children, ...other }: Readonly<CustomNestedMenuItemProps>) {
     const [subMenuActive, setSubMenuActive] = useState(false);
-    const [menuPosition, setMenuPosition] = useState<'left' | 'right'>('right');
-    const parentRef = useRef<HTMLLIElement | null>(null);
-    const submenuRef = useRef<HTMLDivElement | null>(null);
 
-    useEffect(() => {
-        if (parentRef.current && submenuRef.current) {
-            const parentRect = parentRef.current.getBoundingClientRect();
-            const submenuWidth = submenuRef.current.offsetWidth;
-            const windowWidth = window.innerWidth;
-
-            if (parentRect.right + submenuWidth > windowWidth) {
-                setMenuPosition('left');
-            } else {
-                setMenuPosition('right');
-            }
+    const [shouldLiftLeft, setShouldLiftLeft] = useState<boolean>(false);
+    const containerRef = useRef<HTMLElement | null>(null);
+    const calculateSubmenuPosition = useCallback(() => {
+        if (containerRef.current) {
+            const containerRect = containerRef.current.getBoundingClientRect();
+            // for simplicity and keep all items have the same behavior,
+            // take the width of the item as the estimated width of submenus
+            const submenuWidth = containerRect.width;
+            const availableSpaceToRight = window.innerWidth - containerRect.right;
+            // should lift to left or not
+            setShouldLiftLeft(submenuWidth > availableSpaceToRight);
         }
-    }, [children]);
-
-    const anchorOrigin: { vertical: 'top' | 'center' | 'bottom'; horizontal: 'left' | 'center' | 'right' } = {
-        vertical: 'top',
-        horizontal: menuPosition === 'right' ? 'right' : 'left',
-    };
-
-    const transformOrigin: { vertical: 'top' | 'center' | 'bottom'; horizontal: 'left' | 'center' | 'right' } = {
-        vertical: 'top',
-        horizontal: menuPosition === 'right' ? 'left' : 'right',
-    };
+    }, []);
 
     return (
         <NestedMenuItem
+            onMouseEnter={calculateSubmenuPosition}
             {...other}
             parentMenuOpen
+            ContainerProps={{ ref: containerRef }}
             MenuProps={{
-                anchorOrigin,
-                transformOrigin,
-                PaperProps: { ref: submenuRef },
+                anchorOrigin: {
+                    vertical: 'top',
+                    horizontal: shouldLiftLeft ? 'left' : 'right',
+                },
+                transformOrigin: {
+                    vertical: 'top',
+                    horizontal: shouldLiftLeft ? 'right' : 'left',
+                },
             }}
             sx={mergeSx(subMenuActive ? styles.highlightedParentLine : styles.highlightedLine, sx)}
         >
