e4655bef7e8f95f8e34d7020f4f2791e8cc1b6a5
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-11-12 09:50:34 +0100

    add property

--- src/utils/types/metadata.ts
+++ src/utils/types/metadata.ts
@@ -21,6 +21,11 @@ export type Metadata = {
 
 type ThemeColors = Record<string, string>;
 
+type SldAndNadColors = {
+    darkThemeColors: ThemeColors;
+    lightThemeColors: ThemeColors;
+};
+
 export type BaseVoltageConfig = {
     name: string;
     vlValue: number;
@@ -28,8 +33,7 @@ export type BaseVoltageConfig = {
     maxValue: number;
     label: string;
     mapColor: string;
-    darkThemeColors: ThemeColors;
-    lightThemeColors: ThemeColors;
+    sldAndNadColors: SldAndNadColors;
     profile: string;
 };
 
@@ -44,5 +48,5 @@ export type StudyMetadata = Metadata & {
     defaultCountry?: string;
     favoriteCountries?: string[];
     substationPropertiesGlobalFilters?: Map<string, string[]>; // used to generate user specific global filters
-    baseVoltagesConfig?: BaseVoltageConfig[];
+    baseVoltagesConfig?: BaseVoltageConfig[]; // use to define the voltage ranges and their colors
 };
