8584f5b041500cafa03f454f29f289556b83e2b6
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2025-12-16 10:41:14 +0100

    Fixes

--- src/components/parameters/sensi/use-factor-count-display.tsx
+++ src/components/parameters/sensi/use-factor-count-display.tsx
@@ -1,7 +1,13 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 import { useMemo } from 'react';
 import { Box, CircularProgress } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
-import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import { ErrorOutline as ErrorOutlineIcon } from '@mui/icons-material';
 import type { MuiStyles } from '../../../utils';
 
 const styles = {
