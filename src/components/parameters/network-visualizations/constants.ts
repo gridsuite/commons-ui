ea68ded0d2a3b6afe23c18d97f4b1eb7bccd73d8
Etienne LESOT <etienne.lesot@rte-france.com>
2025-12-16 16:45:10 +0100

    fix lint
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/parameters/network-visualizations/constants.ts
+++ src/components/parameters/network-visualizations/constants.ts
@@ -4,10 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import {
-    NadPositionsGenerationMode,
-    SubstationLayout,
-} from './network-visualizations.types';
+import { NadPositionsGenerationMode, SubstationLayout } from './network-visualizations.types';
 
 // Defined in powsybl/network-viewer, duplicated here
 export enum LineFlowMode {
