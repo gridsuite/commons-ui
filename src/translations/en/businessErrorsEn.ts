01f2d5b3a430d58d424df749ccff174912c41683
basseche <bassel.el-cheikh_externe@rte-france.com>
2026-01-23 16:46:04 +0100

    Add business messages translations for study server (#988)
    ---------
    
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/translations/en/businessErrorsEn.ts
+++ src/translations/en/businessErrorsEn.ts
@@ -39,6 +39,7 @@ export const businessErrorsEn = {
     'study.networkExportFailed': 'Failed to export network.',
     'study.tooManyNadConfigs': 'Maximum number of NAD configuration exceeded.',
     'study.tooManyMapCards': 'Maximum number of cards exceeded.',
+    'study.elementAlreadyExists': 'An element with the same name already exists',
     'useradmin.permissionDenied': "You don't have permission to perform this action.",
     'useradmin.userNotFound': 'User not found.',
     'useradmin.userAlreadyExists': 'User already exists.',
