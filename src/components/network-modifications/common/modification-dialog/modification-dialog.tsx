0b75373d171840ddf8c7d41686699ed078ec9ab0
David BRAQUART <david.braquart@rte-france.com>
2026-01-30 19:18:19 +0100

    report from grid-study "Add modification submit button data test id (#3703)"
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/network-modifications/common/modification-dialog/modification-dialog.tsx
+++ src/components/network-modifications/common/modification-dialog/modification-dialog.tsx
@@ -71,6 +71,7 @@ export function ModificationDialog<TFieldValues extends FieldValues>({
 
     const submitButton = (
         <SubmitButton
+            data-testid="ValidateButton"
             onClick={handleSubmit(handleValidate, handleValidationError)}
             variant="outlined"
             disabled={disabledSave}
