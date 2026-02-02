18c961aa2a02843fe62c2333d7c0858b55eaee4e
Thang PHAM <phamthang37@gmail.com>
2026-02-02 23:37:48 +0100

    translation
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/translations/fr/businessErrorsFr.ts
+++ src/translations/fr/businessErrorsFr.ts
@@ -73,6 +73,6 @@ export const businessErrorsFr = {
     'sensitivityAnalysis.tooManyFactors':
         'Trop de facteurs pour exécuter l’analyse de sensibilité : {resultCount} résultats (limite : {resultCountLimit}) et {variableCount} variables (limite : {variableCountLimit}).',
     'pccMin.missingFilter': 'La configuration contient un filtre qui a été supprimé.',
-    'dynamicMarginCalculation.providerNotFound': 'Simulateur du calcul dynamique de marge non trouvé.',
+    'dynamicMarginCalculation.providerNotFound': 'Simulateur du calcul de marge dynamique non trouvé.',
     'dynamicMarginCalculation.loadFilterNotFound': "Certains filtres de consommations n'existent pas : {filterUuids}",
 };
