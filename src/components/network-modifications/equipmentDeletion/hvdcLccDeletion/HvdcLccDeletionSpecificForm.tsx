5f2aab35e42a200eabb2223a7a26ecee9a2779b4
David BRAQUART <david.braquart@rte-france.com>
2026-03-10 18:38:17 +0100

    linter (move sub-component outside)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/components/network-modifications/equipmentDeletion/hvdcLccDeletion/HvdcLccDeletionSpecificForm.tsx
+++ src/components/network-modifications/equipmentDeletion/hvdcLccDeletion/HvdcLccDeletionSpecificForm.tsx
@@ -20,6 +20,31 @@ interface ShuntCompensatorSelectionFormProps {
     mcsRows: Record<'id', string>[];
 }
 
+function ShuntCompensatorSelectionForm({ title, arrayFormName, mcsRows }: Readonly<ShuntCompensatorSelectionFormProps>) {
+    return (
+        <Grid item container spacing={1} direction="column">
+            <Grid item>
+                <h4>
+                    <FormattedMessage id={title} />
+                </h4>
+            </Grid>
+            {mcsRows.map((field, index) => (
+                <Grid container spacing={1} alignItems="center" key={field.id}>
+                    <Grid item xs={1} alignItems="start">
+                        <CheckboxInput
+                            key={`${field.id}SEL`}
+                            name={`${arrayFormName}[${index}].${FieldConstants.SHUNT_COMPENSATOR_SELECTED}`}
+                        />
+                    </Grid>
+                    <Grid item xs={11} alignItems="start">
+                        <ReadOnlyInput key={`${field.id}ID`} name={`${arrayFormName}[${index}].${FieldConstants.ID}`} />
+                    </Grid>
+                </Grid>
+            ))}
+        </Grid>
+    );
+}
+
 export function HvdcLccDeletionSpecificForm() {
     const { fields: mcsRows1 } = useFieldArray({
         name: `${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_1}`,
@@ -28,56 +53,24 @@ export function HvdcLccDeletionSpecificForm() {
         name: `${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_2}`,
     });
 
-    function ShuntCompensatorSelectionForm({ title, arrayFormName, mcsRows }: ShuntCompensatorSelectionFormProps) {
-        return (
-            <Grid item container spacing={1} direction="column">
-                <Grid item>
-                    <h4>
-                        <FormattedMessage id={title} />
-                    </h4>
-                </Grid>
-                {mcsRows.map((field, index) => (
-                    <Grid container spacing={1} alignItems="center" key={field.id}>
-                        <Grid item xs={1} alignItems="start">
-                            <CheckboxInput
-                                key={`${field.id}SEL`}
-                                name={`${arrayFormName}[${index}].${FieldConstants.SHUNT_COMPENSATOR_SELECTED}`}
-                            />
-                        </Grid>
-                        <Grid item xs={11} alignItems="start">
-                            <ReadOnlyInput
-                                key={`${field.id}ID`}
-                                name={`${arrayFormName}[${index}].${FieldConstants.ID}`}
-                            />
-                        </Grid>
-                    </Grid>
-                ))}
-            </Grid>
-        );
-    }
-
-    const mcsOnsideOne = (
-        <ShuntCompensatorSelectionForm
-            title="Side1"
-            arrayFormName={`${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_1}`}
-            mcsRows={mcsRows1}
-        />
-    );
-
-    const mcsOnsideTwo = (
-        <ShuntCompensatorSelectionForm
-            title="Side2"
-            arrayFormName={`${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_2}`}
-            mcsRows={mcsRows2}
-        />
-    );
-
     return (
         <Grid container spacing={1} direction="column" paddingTop={2} paddingLeft={1}>
             <GridSection title="LCCConverterStationShuntCompensators" />
             <Grid container spacing={1}>
-                <GridItem>{mcsOnsideOne}</GridItem>
-                <GridItem>{mcsOnsideTwo}</GridItem>
+                <GridItem>
+                    <ShuntCompensatorSelectionForm
+                        title="Side1"
+                        arrayFormName={`${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_1}`}
+                        mcsRows={mcsRows1}
+                    />
+                </GridItem>
+                <GridItem>
+                    <ShuntCompensatorSelectionForm
+                        title="Side2"
+                        arrayFormName={`${FieldConstants.DELETION_SPECIFIC_DATA}.${FieldConstants.SHUNT_COMPENSATOR_SIDE_2}`}
+                        mcsRows={mcsRows2}
+                    />
+                </GridItem>
             </Grid>
         </Grid>
     );
