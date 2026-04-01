54378d5ef320807937e8cf3706372e5da9abdffe
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2026-04-01 11:47:39 +0200

    Add business error message for missing filters or contingencies list in sensitivity-analysis (#1081)
    * Add business error message for missing filters or contingencies lists in sensitivity-analysis
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/translations/en/businessErrorsEn.ts
+++ src/translations/en/businessErrorsEn.ts
@@ -76,6 +76,8 @@ export const businessErrorsEn = {
         'Only last rule can have empty filter: type {equipmentType}, rule index {index}.',
     'sensitivityAnalysis.tooManyFactors':
         'Too many factors to run sensitivity analysis: {resultCount} results (limit: {resultCountLimit}) and {variableCount} variables (limit: {variableCountLimit}).',
+    'sensitivityAnalysis.filtersOrContingenciesListsNotFound':
+        'The configuration contains one or more contingencies lists or filters that have been deleted.',
     'pccMin.missingFilter': 'The configuration contains one filter that has been deleted.',
     'pccMin.voltageLevelsLimitExceeded':
         'The result of filter evaluation gives {voltageLevelSize} voltage levels, which exceeds the limit {voltageLevelsLimit}.',
