eafeb1bb02c78927810127a2dee92a3019f55d79
Joris Mancini <joris.mancini_externe@rte-france.com>
2026-04-02 16:19:03 +0200

    feat(network-modifications): introduce useTabsWithError hook and integrate in tab-based forms
    - Add useTabsWithError hook that manages tab navigation with automatic error
      highlighting for react-hook-form based tabbed forms
    - Hook computes tabIndexesWithError inline (not in state) so tab error styles
      update immediately when errors are cleared
    - Auto-navigation to first errored tab is triggered on submit (submitCount) to
      avoid mid-input tab jumps during on-change revalidation
    - Integrate hook in VoltageLevelCreationForm replacing manual useState/useEffect
      error tracking with VOLTAGE_LEVEL_TAB_FIELDS constant
    - Integrate hook in LoadForm replacing LoadDialogTabs/Header/Content components
      with direct tab rendering using LOAD_TAB_FIELDS constant
    
    Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/components/network-modifications/load/common/LoadForm.tsx
+++ src/components/network-modifications/load/common/LoadForm.tsx
@@ -5,14 +5,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
-import { useFormState } from 'react-hook-form';
-import { LoadDialogTabsContent, LoadDialogTabsContentProps } from './LoadDialogTabsContent';
+import { LOAD_TAB_FIELDS, LoadDialogTab } from './load.utils';
+import { useTabsWithError } from '../../../../hooks';
 import { LoadDialogHeader, LoadDialogHeaderProps } from './LoadDialogHeader';
-import { LoadDialogTab } from './load.utils';
-import { FieldConstants } from '../../../../utils';
 import { LoadDialogTabs } from './LoadDialogTabs';
+import { LoadDialogTabsContent, LoadDialogTabsContentProps } from './LoadDialogTabsContent';
 
 interface LoadFormProps
     extends LoadDialogHeaderProps,
@@ -25,30 +23,10 @@ export function LoadForm({
     fetchBusesOrBusbarSections,
     PositionDiagramPane,
 }: Readonly<LoadFormProps>) {
-    const [tabIndexesWithError, setTabIndexesWithError] = useState<number[]>([]);
-    const [tabIndex, setTabIndex] = useState<number>(LoadDialogTab.CONNECTIVITY_TAB);
-
-    const { errors } = useFormState();
-
-    useEffect(() => {
-        const tabsInError: number[] = [];
-        if (errors?.[FieldConstants.CONNECTIVITY] !== undefined) {
-            tabsInError.push(LoadDialogTab.CONNECTIVITY_TAB);
-        }
-        if (
-            errors?.[FieldConstants.ACTIVE_POWER_SET_POINT] !== undefined ||
-            errors?.[FieldConstants.REACTIVE_POWER_SET_POINT] !== undefined ||
-            errors?.[FieldConstants.ADDITIONAL_PROPERTIES] !== undefined
-        ) {
-            tabsInError.push(LoadDialogTab.CHARACTERISTICS_TAB);
-        }
-        if (tabsInError.length > 0) {
-            setTabIndex((currentTabIndex) => {
-                return tabsInError.includes(currentTabIndex) ? currentTabIndex : tabsInError[0];
-            });
-        }
-        setTabIndexesWithError(tabsInError);
-    }, [errors]);
+    const { tabIndex, setTabIndex, tabIndexesWithError } = useTabsWithError<LoadDialogTab>(
+        LOAD_TAB_FIELDS,
+        LoadDialogTab.CONNECTIVITY_TAB
+    );
 
     return (
         <Grid container direction="column" spacing={2}>
