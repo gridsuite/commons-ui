26586e40257686cc2556f0b7e9cbc404dcae5fac
EtienneLt <etienne.lesot@rte-france.com>
2026-04-24 14:27:48 +0200

    remove hvdc option for icc clusters (#1109)
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/parameters/short-circuit/short-circuit-icc-cluster-table-cell.tsx
+++ src/components/parameters/short-circuit/short-circuit-icc-cluster-table-cell.tsx
@@ -45,7 +45,7 @@ export function ShortCircuitIccClusterTableCell({
                     size="small"
                     name={`${formName}[${rowIndex}].${column.dataKey}`}
                     label={column.titleId}
-                    options={['GENERATOR', 'HVDC']}
+                    options={['GENERATOR']}
                     disabled={inputsDisabled}
                 />
             )}
