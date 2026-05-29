6142cbf99ac29a8092af45aa1597fcca49070d30
David BRAQUART <david.braquart@rte-france.com>
2026-05-29 12:32:11 +0200

    sounds ok
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

---
+++ src/features/network-modifications/battery/modification/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+export * from './batteryModification.types';
+export * from './batteryModification.utils';
+export * from './batteryTabs.utils';
+export * from './BatteryDialogHeader';
+export * from './BatteryDialogTabs';
+export * from './BatteryDialogTabsContent';
+export * from './BatteryModificationForm';
