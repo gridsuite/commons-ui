245857d7c2472abae2c2bcca76036178ec984b94
Etienne LESOT <etienne.lesot@rte-france.com>
2026-06-01 08:19:49 +0200

    review
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/components/parameters/short-circuit/short-circuit-parameters.type.ts
+++ src/components/parameters/short-circuit/short-circuit-parameters.type.ts
@@ -39,7 +39,7 @@ export interface FormPowerElectronicsCluster extends BasePowerElectronicsCluster
     filters: FilterPOJO[];
 }
 export interface PowerElectronicsCluster extends BasePowerElectronicsCluster {
-    filters: string[];
+    filters: UUID[];
 }
 
 export interface ShortCircuitParametersDto {
