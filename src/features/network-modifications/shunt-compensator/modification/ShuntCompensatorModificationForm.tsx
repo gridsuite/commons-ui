33a36c80dcb44a2141d5ca67fcd750566e1647c2
achour94 <berrahmaachour@gmail.com>
2026-06-01 11:20:03 +0200

    Move shunt compensator modification form
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

---
+++ src/features/network-modifications/shunt-compensator/modification/ShuntCompensatorModificationForm.tsx
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import { Grid, TextField } from '@mui/material';
+import { useWatch } from 'react-hook-form';
+import { TextInput } from '../../../../components/ui';
+import GridSection from '../../../../components/composite/grid/grid-section';
+import { ConnectivityForm } from '../../common/connectivity/ConnectivityForm';
+import { ConnectivityNetworkProps } from '../../common/connectivity/connectivity.type';
+import { PropertiesForm } from '../../common/properties/PropertiesForm';
+import { PowerMeasurementsForm } from '../../common/measurements/PowerMeasurementsForm';
+import { filledTextField } from '../../common';
+import { FieldConstants } from '../../../../utils';
+import { CharacteristicsForm } from '../common/CharacteristicsForm';
+import { ShuntCompensatorFormInfos } from '../common/shuntCompensator.types';
+
+export interface ShuntCompensatorModificationFormProps extends ConnectivityNetworkProps {
+    shuntCompensatorToModify?: ShuntCompensatorFormInfos | null;
+}
+
+export function ShuntCompensatorModificationForm({
+    shuntCompensatorToModify,
+    voltageLevelOptions = [],
+    fetchBusesOrBusbarSections,
+    PositionDiagramPane,
+}: Readonly<ShuntCompensatorModificationFormProps>) {
+    const equipmentId = useWatch({ name: FieldConstants.EQUIPMENT_ID });
+
+    return (
+        <Grid container direction="column" spacing={2}>
+            <Grid item>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <TextField
+                            size="small"
+                            fullWidth
+                            label="ID"
+                            value={equipmentId ?? ''}
+                            InputProps={{ readOnly: true }}
+                            disabled
+                            {...filledTextField}
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextInput
+                            name={FieldConstants.EQUIPMENT_NAME}
+                            label="Name"
+                            formProps={filledTextField}
+                            previousValue={shuntCompensatorToModify?.name}
+                            clearable
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <GridSection title="Connectivity" />
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <ConnectivityForm
+                            isEquipmentModification
+                            previousValues={shuntCompensatorToModify ?? undefined}
+                            voltageLevelOptions={voltageLevelOptions}
+                            PositionDiagramPane={PositionDiagramPane}
+                            fetchBusesOrBusbarSections={fetchBusesOrBusbarSections}
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <GridSection title="Characteristics" />
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <CharacteristicsForm previousValues={shuntCompensatorToModify ?? undefined} isModification />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <GridSection title="MeasurementsSection" />
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <PowerMeasurementsForm
+                            reactivePowerMeasurement={shuntCompensatorToModify?.measurementQ}
+                            reactivePowerOnly
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <PropertiesForm networkElementType="shuntCompensator" isModification />
+            </Grid>
+        </Grid>
+    );
+}
