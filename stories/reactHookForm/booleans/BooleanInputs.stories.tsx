7e94c20fe871f7056c6f16a04d4252e1744e907f
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-06-23 11:50:00 +0200

    add licence headers
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- stories/reactHookForm/booleans/BooleanInputs.stories.tsx
+++ stories/reactHookForm/booleans/BooleanInputs.stories.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import type { PropsWithChildren } from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Checkbox, Switch } from '@mui/material';
