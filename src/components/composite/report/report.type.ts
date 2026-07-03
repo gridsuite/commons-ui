8ab8969f5d749033bafde809af7048295c4c1b6e
Franck LECUYER <franck.lecuyer@rte-france.com>
2026-07-03 09:12:56 +0200

    Fix invalid license header
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/components/composite/report/report.type.ts
+++ src/components/composite/report/report.type.ts
@@ -1,12 +1,12 @@
-import { NETWORK_MODIFICATION } from './report.constant';
-import { ComputingType } from '../../../utils';
-
 /**
  * Copyright (c) 2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
+import { NETWORK_MODIFICATION } from './report.constant';
+import { ComputingType } from '../../../utils';
 
 export type SeverityLevel = 'UNKNOWN' | 'TRACE' | 'DEBUG' | 'DETAIL' | 'INFO' | 'WARN' | 'ERROR' | 'FATAL';
 
