0ce98954c929b8c87f6fd6d516aaa66a697415fa
Etienne LESOT <etienne.lesot@rte-france.com>
2026-07-22 15:37:47 +0200

    remove qpercent from battery creation dialog
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/features/network-modifications/battery/creation/BatteryCreationForm.tsx
+++ src/features/network-modifications/battery/creation/BatteryCreationForm.tsx
@@ -80,6 +80,7 @@ export function BatteryCreationForm({
         <VoltageRegulationForm
             voltageLevelOptions={voltageLevelOptions}
             fetchVoltageLevelEquipments={fetchVoltageLevelEquipments}
+            isGenerator={false}
         />
     );
 
