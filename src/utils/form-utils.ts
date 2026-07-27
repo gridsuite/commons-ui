2c2585e6c6d7b5b8e729077ab35d7a4c2d509c62
Caroline Jeandat <caroline.jeandat@rte-france.com>
2026-07-27 17:57:18 +0200

    export function

--- src/utils/form-utils.ts
+++ src/utils/form-utils.ts
@@ -3,7 +3,8 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */ import { FieldErrors } from 'react-hook-form';
+ */
+import { FieldErrors } from 'react-hook-form';
 import { FieldConstants } from './constants';
 
 // when UniqueNameInput is used in a form, we need to disable the validation button while the name is being validated
