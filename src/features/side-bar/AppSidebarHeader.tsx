08ab8d51c4c0e5bb4c49f0c1e0b4b7a6bcbece56
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-20 11:15:54 +0200

    add copyright headers
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/features/side-bar/AppSidebarHeader.tsx
+++ src/features/side-bar/AppSidebarHeader.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { Box, Divider, Stack, Typography, IconButton, Tooltip } from '@mui/material';
 import { Info } from '@mui/icons-material';
 import { ReactNode, useState } from 'react';
--- src/features/side-bar/footer/settings/LanguageSelection.tsx
+++ src/features/side-bar/footer/settings/LanguageSelection.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { Done } from '@mui/icons-material';
 import { FormattedMessage } from 'react-intl';
 import { GsLang } from '../../../../utils';
