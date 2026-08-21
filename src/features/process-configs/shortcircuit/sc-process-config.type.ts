e2f7a3f98f7648c7aa721188ecb0cc0ac357c4ed
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2026-08-21 13:10:10 +0200

    Edit short circuit process configuration (#1283)
    * Edit short circuit process configuration
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ src/features/process-configs/shortcircuit/sc-process-config.type.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { UUID } from 'node:crypto';
+import { ProcessConfigBaseBackend, ProcessType } from '../common';
+
+export interface ShortcircuitProcessConfigBackend extends ProcessConfigBaseBackend {
+    processType: ProcessType.SHORT_CIRCUIT;
+    shortCircuitParametersUuid: UUID;
+}
