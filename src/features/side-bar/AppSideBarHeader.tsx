75e9673658b2e2d7fbdf117a3c5f67a5181ed58c
Radouane Khouadri <redouane.khouadri_externe@rte-france.com>
2026-08-25 11:09:05 +0200

    add env chip over logo

--- src/features/side-bar/AppSideBarHeader.tsx
+++ src/features/side-bar/AppSideBarHeader.tsx
@@ -5,11 +5,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import { Box, Divider, Stack, Typography, IconButton, Tooltip, ThemeProvider } from '@mui/material';
+import { Box, Divider, Stack, Typography, IconButton, Tooltip, ThemeProvider, Chip } from '@mui/material';
 import { Info } from '@mui/icons-material';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useMemo, useState } from 'react';
 import { AboutDialog, GridSuiteModule } from '../topBar';
 import { useAppSideBarTheme } from './AppSideBarThemeProvider';
+import { fetchEnv, Environment } from '../../services';
 
 interface AppSideBarHeaderProps {
     isMinimized: boolean;
@@ -22,6 +23,14 @@ interface AppSideBarHeaderProps {
     globalVersionPromise: () => Promise<string>;
     additionalModulesPromise: () => Promise<GridSuiteModule[]>;
 }
+
+const envColors: Record<Environment, string> = {
+    REC: '#304FFE',
+    DEV: '#DD2C00',
+    PRE: '#AA00FF',
+    PRO: '#DD2C00',
+    DCH: '#2E7D32',
+};
 
 export function AppSideBarHeader({
     isMinimized,
@@ -36,6 +45,37 @@ export function AppSideBarHeader({
 }: Readonly<AppSideBarHeaderProps>) {
     const { theme } = useAppSideBarTheme();
     const [isAboutDialogOpen, setIsAboutDialogOpen] = useState(false);
+    const [environment, setEnvironment] = useState<Environment | string>('');
+
+    useEffect(() => {
+        fetchEnv().then((env) => setEnvironment(env?.environment ?? ''));
+    }, []);
+
+    const chip = useMemo(() => {
+        if (!(environment in envColors)) {
+            return null;
+        }
+        return (
+            <Chip
+                label={environment}
+                size="small"
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    right: 5,
+                    transform: isMinimized ? 'translate(40%, -10%)' : 'translate(120%, -10%)',
+                    fontSize: '0.5rem',
+                    backgroundColor: envColors[environment as Environment],
+                    color: '#ffffff',
+                    height: isMinimized ? '12px' : '15px',
+                    '& .MuiChip-label': {
+                        padding: '0 4px',
+                    },
+                }}
+            />
+        );
+    }, [environment, isMinimized]);
+
     return (
         <>
             <Stack
@@ -52,17 +92,23 @@ export function AppSideBarHeader({
                         justifyContent: isMinimized ? 'center' : 'normal',
                     }}
                 >
-                    {appLogo}
+                    <Box sx={{ position: 'relative', display: 'inline-block' }}>
+                        {appLogo}
+                        {isMinimized && chip}
+                    </Box>
+
                     {!isMinimized && (
-                        <Typography variant="h6">
-                            Grid
-                            <Box component="span" sx={{ color: appNameColor }}>
-                                {appName}
-                            </Box>
-                        </Typography>
+                        <Box sx={{ position: 'relative', display: 'inline-block' }}>
+                            <Typography variant="h6">
+                                Grid
+                                <Box component="span" sx={{ color: appNameColor }}>
+                                    {appName}
+                                </Box>
+                            </Typography>
+                            {chip}
+                        </Box>
                     )}
                 </Stack>
-
                 <Stack
                     width="100%"
                     direction="row"
@@ -75,14 +121,12 @@ export function AppSideBarHeader({
                     justifyContent={isMinimized ? 'center' : 'end'}
                     spacing={1}
                 >
-                    <>
-                        {!isMinimized && <Typography variant="caption">V{appVersion}</Typography>}
-                        <Tooltip title={`V${appVersion}`} disableHoverListener={!isMinimized}>
-                            <IconButton sx={{ padding: 0 }} onClick={() => setIsAboutDialogOpen(true)}>
-                                <Info fontSize="small" color="disabled" />
-                            </IconButton>
-                        </Tooltip>
-                    </>
+                    {!isMinimized && <Typography variant="caption">V{appVersion}</Typography>}
+                    <Tooltip title={`V${appVersion}`} disableHoverListener={!isMinimized}>
+                        <IconButton sx={{ padding: 0 }} onClick={() => setIsAboutDialogOpen(true)}>
+                            <Info fontSize="small" color="disabled" />
+                        </IconButton>
+                    </Tooltip>
                 </Stack>
 
                 <Divider />
