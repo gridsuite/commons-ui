16fd3c39a190c809ce2aa21e18de57211330aa03
David BRAQUART <david.braquart@rte-france.com>
2026-08-27 20:36:30 +0200

    import type uuid
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/features/network-modifications/hvdcLine/vsc/creation/vscHvdcLineCreation.types.ts
+++ src/features/network-modifications/hvdcLine/vsc/creation/vscHvdcLineCreation.types.ts
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { UUID } from 'node:crypto';
+import type { UUID } from 'node:crypto';
 import { ModificationType } from '../../../../../utils';
 import { Property } from '../../../common/properties/properties.type';
 import { ReactiveCapabilityCurvePoints } from '../../../common/reactiveLimits/reactiveLimits.type';
