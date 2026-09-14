33c4dfe5e05ddd3be4b9cc3821377bd22fadb847
sBouzols <sylvain.bouzols@gmail.com>
2026-09-14 11:35:07 +0200

    style(EquipmentSelectionParameters): reduce top margin beetween parameters
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/features/parameters/voltage-init/equipment-selection-parameters.tsx
+++ src/features/parameters/voltage-init/equipment-selection-parameters.tsx
@@ -27,7 +27,7 @@ const equipmentsSelectionStyles = {
         marginBottom: theme.spacing(2),
     }),
     choice: (theme) => ({
-        marginTop: theme.spacing(4),
+        marginTop: theme.spacing(2),
     }),
     choiceContainer: {
         paddingLeft: 1,
