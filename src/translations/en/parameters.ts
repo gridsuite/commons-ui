4aa087ba41751fb848ee8196efef13c01fd73dc1
EtienneLt <etienne.lesot@rte-france.com>
2026-09-15 14:41:32 +0200

    Update src/translations/en/parameters.ts
    Co-authored-by: Sylvain Bouzols <sylvain.bouzols@gmail.com>

--- src/translations/en/parameters.ts
+++ src/translations/en/parameters.ts
@@ -124,7 +124,7 @@ export const parametersEn = {
     ShortCircuitCharacteristics: 'Features considered',
     ShortCircuitVoltageProfileMode: 'Initial voltage profile',
     ShortCircuitStartedGeneratorsMode: 'Generators started',
-    ShortCircuitInClusterFilter: 'Definition of cluster filters',
+    ShortCircuitInClusterFilter: 'Definition of calculation cluster filters',
     ShortCircuitInCluster: 'In cluster',
     ShortCircuitOutCluster: 'Out cluster',
     startedGeneratorsInCalculationClusterThreshold: 'Threshold',
